07f8d18b2b77f07c33c948d652ee0d97a8fa486e
keshav72727 <75607598+keshav72727@users.noreply.github.com>
2021-05-07 17:09:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,7 +47,7 @@ function draw() {
     else if(keyDown(UP_ARROW)){
       updateHeight(0,-10);
       balloon.addAnimation("hotAirBalloon",balloonImage2);
-      balloon.scale = balloon.scale -0.01;
+      balloon.scale = balloon.scale +0.01;
     }
     else if(keyDown(DOWN_ARROW)){
       updateHeight(0,10);
